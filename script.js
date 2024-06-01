92b852c41d7971a36a24f8ec8df09577e4129f0f
Fudge-Lickels <107988982+Fudge-Lickels@users.noreply.github.com>
2024-06-01 11:39:27 +0200

    Update infinity alert

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const divide = (num1, num2) => {
   if (num2 !== 0) {
     return num1 / num2;
   }
-  alert('Cannot divide by zero');
+  alert('Numbers divided by zero are equal to infinity');
   return;
 };
 
